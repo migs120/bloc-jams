93230d4ff9b87e05be3f49ac57cfd59095cc73be
Your Name <miggymigs@migs.com>
2016-02-07 13:32:15 -0800

    assignment-22

--- public/scripts/landing.js
+++ public/scripts/landing.js
@@ -1,30 +1,28 @@
- var animatePoints = function() {
- 
-     var points = document.getElementsByClassName('point');
- 
-     var revealFirstPoint = function() {
-         points[0].style.opacity = 1;
-         points[0].style.transform = "scaleX(1) translateY(0)";
-         points[0].style.msTransform = "scaleX(1) translateY(0)";
-         points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
- 
-     var revealSecondPoint = function() {
-         points[1].style.opacity = 1;
-         points[1].style.transform = "scaleX(1) translateY(0)";
-         points[1].style.msTransform = "scaleX(1) translateY(0)";
-         points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
- 
-     var revealThirdPoint = function() {
-         points[2].style.opacity = 1;
-         points[2].style.transform = "scaleX(1) translateY(0)";
-         points[2].style.msTransform = "scaleX(1) translateY(0)";
-         points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
- 
-     revealFirstPoint();
-     revealSecondPoint();
-     revealThirdPoint();
- 
- };
+var revealPoint = function(){
+   var pnt = document.querySelectorAll(".point");
+    for(var i = 0; i< pnt.length; i++){
+       //function() {
+         pnt[i].style.opacity = 1;
+         pnt[i].style.transform = "scaleX(1) translateY(0)";
+         pnt[i].style.msTransform = "scaleX(1) translateY(0)";
+         pnt[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    // }   
+    }
+    
+    
+}
+
+setTimeout(revealPoint(),90000000);
+
+
+
+
+
+
+
+
+
+
+
+
+
