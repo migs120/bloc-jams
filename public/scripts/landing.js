88c64f916acbbea6a732db3d03a41d340b2725a9
Your Name <miggymigs@migs.com>
2016-02-08 07:11:20 -0800

    checkpoint-23-dom-events

--- public/scripts/landing.js
+++ public/scripts/landing.js
@@ -1,23 +1,40 @@
-var revealPoint = function(){
+var animatePoints = function(){
+
+var revealPoints = function(){
    var pnt = document.querySelectorAll(".point");
     for(var i = 0; i< pnt.length; i++){
-       //function() {
+     
          pnt[i].style.opacity = 1;
          pnt[i].style.transform = "scaleX(1) translateY(0)";
          pnt[i].style.msTransform = "scaleX(1) translateY(0)";
-         pnt[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-    // }   
-    }
-    
-    
+         pnt[i].style.WebkitTransform = "scaleX(1) translateY(0)";  }  } 
+revealPoints();
+
 }
 
-setTimeout(revealPoint(),90000000);
+
+//window.onload(
+//setTimeout(revealPoints(),3000);
+
+//);
 
 
+ window.onload = function() {
+    if (window.innerHeight > 950) {
+                animatePoints();
+                    }
+     
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
-
-
+     window.addEventListener("scroll", function(event) {
+      
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(//pointsArray
+                          );   
+         }
+     });
+ }
 
 
 
