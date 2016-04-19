bb8d66472b790b22239e8620fec37bf1dd5ed5b4
Your Name <you@example.com>
2016-04-18 22:19:10 -0700

    server6

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 var express = require('express');
 var app = express();
-
+app.set('port', (process.env.PORT || 8080));
 app.use(express.static('public'));
 
 app.get("/", function (req, res) {
@@ -23,7 +23,7 @@ app.get( "/album.html" , function (req, res) {
 })
 
 //var server = app.listen(process.env.PORT || 3000, '0.0.0.0', function () {
-var server = app.listen(process.env.Port || 8080 , function () {
+var server = app.listen(app.get('port') , function () {
   var host = server.address().address
   var port = server.address().port
 
